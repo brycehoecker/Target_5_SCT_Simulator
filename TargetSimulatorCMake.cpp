e806b816010b5981395ad9cea7d2622e5b589a41
brycehoecker <brycehoecker@gmail.com>
2023-10-13 17:15:46 -0700

    Added Reg 0-19.1

--- TargetSimulatorCMake.cpp
+++ TargetSimulatorCMake.cpp
@@ -6,7 +6,14 @@ using namespace std;
 int main(int argc, char **argv) {
 	cout << "Hello World" << endl;
 
-	T5Registers registersArray[177];
+    const int NUM_REGISTERS = 177;
+    T5Registers registersArray[NUM_REGISTERS];
+
+    for (int i = 0; i < NUM_REGISTERS; ++i) {
+        cout << "Register " << (i+1) << ":\n";
+        registersArray[i].printAll();
+        cout << "---------------------------------\n"; // separator for clarity
+    }
 
 	cout << "Version " << TargetSimulatorCMake_VERSION_MAJOR << "." << TargetSimulatorCMake_VERSION_MINOR << endl;
 	return 0;
